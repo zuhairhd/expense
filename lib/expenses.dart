b6bf7f4061d11e2690608c161d777eefc1b4e5a0
Zuhair H Darwish <zuhairhd@gmail.com>
2024-06-06 17:28:56 +0400

    adding expenses widget

---
+++ lib/expenses.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Expenses'),
+      ),
+      body: const Center(
+        child: Text('Expenses'),
+      ),
+    );
+  }
+}
