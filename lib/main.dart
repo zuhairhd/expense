c28a75221716960245fbff935f9150940cfe1393
Zuhair H Darwish <zuhairhd@gmail.com>
2024-06-16 14:33:28 +0400

    after copying the files and remove the errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,66 @@
+import 'package:expnses_ex/widgets/expenses.dart';
 import 'package:flutter/material.dart';
-import 'package:expnses_ex/widget/expenses.dart';
 
-var kColorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
-var kDarkColorScheme =
-    ColorScheme.fromSeed(
-      brightness: Brightness.dark,
-      seedColor: const Color.fromARGB(255, 5, 99, 125),
-      );
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(
     MaterialApp(
       // remove debug banner
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData.dark().copyWith( 
+      darkTheme: ThemeData.dark().copyWith(
+        // useMaterial3: true,
         colorScheme: kDarkColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kDarkColorScheme.onPrimaryContainer,
-          foregroundColor: kDarkColorScheme.primaryContainer,
-          centerTitle: true,
-        ),
         cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.primaryContainer,
-          shadowColor: Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
           ),
-          margin: const EdgeInsets.all(5),
-          elevation: 5,
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.all(3),
-            elevation: 5,
-            shadowColor: kDarkColorScheme.primary,
             backgroundColor: kDarkColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: TextTheme().copyWith(
-          titleLarge: ThemeData().textTheme.titleLarge!.copyWith(
-            fontWeight: FontWeight.bold,
-            // color: kDarkColorScheme.primaryContainer,
-            fontSize: 20,
-          ),
-          bodyMedium: ThemeData().textTheme.bodyMedium!.copyWith(
-            // color: kDarkColorScheme.primaryContainer,
-            fontSize: 16,
-          ),
-          titleSmall: ThemeData().textTheme.titleSmall!.copyWith(
-            // color: kDarkColorScheme.onPrimary,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 14,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
           ),
         ),
       ),
       theme: ThemeData().copyWith(
+        // useMaterial3: true,
         colorScheme: kColorScheme,
-        
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
-          centerTitle: true,
         ),
         cardTheme: const CardTheme().copyWith(
-          // color: kColorScheme.secondaryContainer,
-          // // setting foreground color 
-          //  shadowColor: Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
           ),
-          color: kColorScheme.primaryContainer,
-          
-          margin: const EdgeInsets.all(5),
-          elevation: 5,          
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.all(3),
-            elevation: 5,
-            shadowColor: kColorScheme.primary,
             backgroundColor: kColorScheme.primaryContainer,
           ),
         ),
-        textTheme: TextTheme().copyWith(
-          titleLarge: ThemeData().textTheme.titleLarge!.copyWith(
-            fontWeight: FontWeight.bold,
-      //          foreground: Paint()..color = kColorScheme.primaryContainer,
-            // color: kColorScheme.primaryContainer,
-                // color: Colors.black,
-                fontSize: 20,
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
               ),
-          bodyMedium: ThemeData().textTheme.bodyMedium!.copyWith(
-            // color: kColorScheme.primaryContainer,
-            // color: Colors.black,
-            fontSize: 16,
-          ), 
-          titleSmall: ThemeData().textTheme.titleSmall!.copyWith(
-            // color: kColorScheme.onPrimary,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 14,
-          ),
-        ),
+            ),
       ),
-      themeMode: ThemeMode.system, // ThemeMode.system is default
+      // themeMode: ThemeMode.system, // default
       home: const Expenses(),
     ),
   );
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,23 +1,60 @@
-import 'package:expnses_ex/models/Account.dart';
-// import 'package:flutter/foundation.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
-// import 'package:expnses_ex/models/def-category.dart' show Category; // Specify the correct import for Category class
-// create a new instance of Uuid
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
 const uuid = Uuid();
-final formatedDate = DateFormat.yMMMd();
-// Create a new class called Expense
-class Expense{
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-  // final Category category;
-  // final Category category;
-  final Account category;
-  Expense({required this.title, required this.amount, required this.date, required this.category}):id = uuid.v4();
+  final Category category;
 
- String get formattedDate {
-    return formatedDate.format(date);
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount; // sum = sum + expense.amount
+    }
+
+    return sum;
   }
 }
